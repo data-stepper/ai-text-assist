eb5b83b9a32da520b26d6003ba1bd498977ee1ae
Bent Mueller <bent.hochrad@gmail.com>
2021-09-20 19:58:55 +0200

    Find out how to capture visually selected text parts

--- rplugin/python3/nvim-example-python-plugin.py
+++ rplugin/python3/nvim-example-python-plugin.py
@@ -5,7 +5,7 @@ class Main(object):
     def __init__(self, vim):
         self.vim = vim
 
-    @neovim.function('DoItPython')
+    @neovim.function('DoItPython', range=True)
     def doItPython(self, args):
-        self.vim.command('echo "hello from DoItPython"')
+        self.vim.command('echo "Received {}"'.format(args))
 
